f3c294aa7e91657938816336c712581c8a6bce2e
antukidem <a_ntukidem@yahoo.com>
2016-01-25 22:31:29 +0100

    Update RandomPosts.js

--- RandomPosts.js
+++ RandomPosts.js
@@ -10,9 +10,10 @@ var f=function(w){
 var q,k,m,u,x,p,t,v,r,l="",s=w.feed.entry;
 for(var o=0;o<s.length;o++){
 for(var n=0;n<s[o].link.length;n++){if(s[o].link[n].rel=="alternate"){q=s[o].link[n].href;break}}
-if("media$thumbnail" in s[o]){  
-  var n = s[o].media$thumbnail.url.indexOf("http://res.cloudinary.com/staticcontenthost/image/upload/");
-  if(n >-1) {
+if("media$thumbnail" in s[o]){ 
+  console.log('R: '+s[o].media$thumbnail.url);
+  var ind = s[o].media$thumbnail.url.indexOf("http://res.cloudinary.com/staticcontenthost/image/upload/");
+  if(ind >-1) {
     u=s[o].media$thumbnail.url.replace('http://res.cloudinary.com/staticcontenthost/image/upload/','http://res.cloudinary.com/staticcontenthost/image/upload/w_210,h_180,c_fill/');
   }else{
     u=s[o].media$thumbnail.url.replace(/\/s[0-9]+\-c/g,"/s"+h.ImageSize+"-c");
@@ -21,7 +22,7 @@ if("media$thumbnail" in s[o]){
 {
   u=h.pBlank.replace(/\/s[0-9]+(\-c|\/)/,"/s"+h.ImageSize+"$1");
 }
-console.log('R: '+u);
+
 k=s[o].title.$t;
 at=k.replace("'","/'");
 at=at.replace('"','/"');
