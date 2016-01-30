f4c9cca7d17db7aa78b89db556e4cb8b8994f685
antukidem <a_ntukidem@yahoo.com>
2016-01-30 23:18:58 +0100

    Update RandomPosts.js

--- RandomPosts.js
+++ RandomPosts.js
@@ -51,7 +51,8 @@ function FeaturedPostSide(a){
       m="";//s[o].author[0].name.$t; 
       x="";//r.substring(0,4);
       p="";//r.substring(5,7);t=r.substring(8,10);
-      v="";//h.MonthNames[parseInt(p,10)-1];l+='<li><a target="_blank" href="'+q+'"><div class="overlayx"></div><img class="random" src="'+u+'"/><h4>'+k+'</h4></a><div class="label_text"><span class="date"><span class="dd">'+t+'</span> <span class="dm">'+v+'</span> <span class="dy">'+x+'</span></span> <span class="autname">'+m+"</span></div></li>"
+      v="";//h.MonthNames[parseInt(p,10)-1];
+      /*l+='<li><a target="_blank" href="'+q+'"><div class="overlayx"></div><img class="random" src="'+u+'"/><h4>'+k+'</h4></a><div class="label_text"><span class="date"><span class="dd">'+t+'</span> <span class="dm">'+v+'</span> <span class="dy">'+x+'</span></span> <span class="autname">'+m+"</span></div></li>"*/
       l+='<li><a target="_blank" href="'+q+'"><div class="overlayx"></div><img class="random" src="'+u+'"/><h4>'+k+'</h4></a><div class="label_text"><span class="date"><span class="dd">'+t+'</span> <span class="dm">'+v+'</span></div></li>"
     }
     e("ul",g).append(l).addClass(h.loadingClass)};
