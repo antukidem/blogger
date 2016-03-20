8bf0a37719420d5866b99a766d821c0324969ae6
antukidem <a_ntukidem@yahoo.com>
2016-03-20 09:08:54 +0100

    Update RandomPosts.js

--- RandomPosts.js
+++ RandomPosts.js
@@ -45,13 +45,13 @@ function FeaturedPostSide(a){
       if("media$thumbnail" in s[o]){
         u=s[o].media$thumbnail.url.replace(/\/s[0-9]+\-c/g,"/s"+h.ImageSize+"-c") ;
       }else if(s[o].content!=null){
-           /**/ var m, urls = [], rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;  
+           /**/ var m,firstImgUrl="", urls = [], rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;  
            while ( m = rex.exec( s[o].content.$t ) ) { 
-            urls.push( m[1] ); 
+            firstImgUrl = m[1] ; 
             break;
            } 
-           alert(urls[0]) 
-           var firstImgUrl=urls[0];
+           alert(firstImgUrl) ;
+           //var firstImgUrl=urls[0];
            if(firstImgUrl.length>0) { 
              var n = firstImgUrl.indexOf("http://res.cloudinary.com/staticcontenthost/image/upload/");
              if(n >-1) { 
